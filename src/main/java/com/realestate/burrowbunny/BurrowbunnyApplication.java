05def05e9185482949dc46aad6081aea8db426bc
Xuther001 <xuther@yahoo.com>
2024-10-01 16:21:20 +0700

    initial commit

---
+++ src/main/java/com/realestate/burrowbunny/BurrowbunnyApplication.java
@@ -0,0 +1,13 @@
+package com.realestate.burrowbunny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurrowbunnyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurrowbunnyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/realestate/burrowbunny/BurrowbunnyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realestate.burrowbunny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurrowbunnyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
